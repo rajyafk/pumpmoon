43397fc0aabbdeb8d84eb3943a26fcea11f8b1ba
ahhhhhh <73053375+rajyafk@users.noreply.github.com>
2024-07-01 16:35:43 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-function redirectToPurchasePage() {
-    window.location.href = "https://www.purchasepage.com"; // Replace with your actual purchase page URL
-}
-
-// Optionally, you can add more JavaScript for additional interactivity
